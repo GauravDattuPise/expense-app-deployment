98e7dbb047f1073783d23e222b82ec0a73e511e6
GauravDattuPise <GauravDattuPise>
2023-06-30 13:39:11 +0530

    html file path updated

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/index.html"),
+  res.sendFile(path.join(__dirname, "./client/build/index.html"),
     function (err) {
       res.status(500).send(err);
     }
